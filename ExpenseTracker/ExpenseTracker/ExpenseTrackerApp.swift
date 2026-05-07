35eafd2d764c2b584444f2b28bca02be15577fdb
Unity_one_love <sazanovichmaksim@gmail.com>
2026-05-07 16:29:59 +0300

    chore(color): add colors

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Maksim on 7.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

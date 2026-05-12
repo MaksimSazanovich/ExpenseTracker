7fe9958cc4b77a878a4c2b7080e6b9f12c0be80e
Unity_one_love <sazanovichmaksim@gmail.com>
2026-05-12 16:52:52 +0300

    feat: add chart

--- ExpenseTracker/ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -4,8 +4,11 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListViewModel: TransactionListViewModel
+
     var body: some View {
         NavigationView {
             ScrollView {
@@ -14,6 +17,26 @@ struct ContentView: View {
                     Text("Overview")
                         .font(.title2)
                         .bold()
+                    
+                    // MARK: Chart
+                    let data = transactionListViewModel.accumulateTransactions()
+                    
+                    if !data.isEmpty {
+                        
+                        let totalExpenses = data.last?.1 ?? 0
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                
+                                LineChart()
+                            }
+                            .background(Color(.systemBackground))
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color(.systemBackground), foregroundColor: ColorGradient(.icon.opacity(0.4), .icon)))
+                        .frame(height: 300)
+                        
+                    }
                     
                     // MARK: Transaction List
                     RecentTransactionList()
